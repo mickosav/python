9bdf6dfe90897e543061c3f35ef59d39e0ea6da8
Milutin Savovic <mickosavovic@gmail.com>
2019-10-24 10:47:55 +0200

    Tuples initial

---
+++ tuples_begginer.py
@@ -0,0 +1,21 @@
+# Mutable
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
+list_1[0] = 'Art'
+#list_1.append('Art')
+
+print(list_1)
+print(list_2)
+
+# Immutable
+tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+tuple_2 = tuple_1
+
+tuple_1[0] = 'Art' # -> throws error
+print(tuple_1)
+print(tuple_2)
+
+# tuples don't have as many methods as a list
+# because a lot of list methods involve mutating values, and tuples are immutable
+# other than that, they are pretty much the same as lists
+# (we can loop through them, access values and so on)
