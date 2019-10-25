3a393d4fc82d18e6ec5e19fa4123def86538882d
Milutin Savovic <mickosavovic@gmail.com>
2019-10-25 09:03:52 +0200

    5 - dictionaries

--- 4-tuples.py
+++ 4-tuples.py
@@ -11,7 +11,7 @@ print(list_2)
 tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
 tuple_2 = tuple_1
 
-tuple_1[0] = 'Art' # -> throws error
+# tuple_1[0] = 'Art' # -> throws error
 print(tuple_1)
 print(tuple_2)
 
---
+++ 5-dictionaries.py
@@ -0,0 +1,39 @@
+# In other laguages called hashmaps or associative arrays
+
+# Key must be of a type that is immutable
+# Key can appear in a dictionary only once
+# Almost any type of value can be used as a dictionary key (integer, float, Boolean etc)
+# A tuple can also be a dictionary key, because tuples are immutable
+
+
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci'], 1: 'Hello'}
+
+# accessing values
+print(student['name'])
+print(student[1])
+# ^ if key doesn't exist this will throw exception
+
+print(student.get('sadfasdf')) # -> None
+print(student.get('323radsf', 'Not found')) # -> Not found
+
+student['phone'] = '555-292019'
+student['name'] = 'Micko'
+
+print(student)
+
+student.update({ 'name': 'Pera', (1, 2): 'test' })
+print(student[(1, 2)])
+print(student)
+
+del student[(1, 2)]
+phone = student.pop('phone')
+print(student)
+print(len(student))
+
+print(student.keys()) # just keys
+print(student.values()) # just values
+print(student.items()) # key value pairs
+
+# Looping
+for key, value in student.items():
+  print(key, value)
