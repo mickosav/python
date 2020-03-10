e7c2578533d162c9393f0d9fc0c008cd786eb321
Milutin Savović <mickosavovic@gmail.com>
2020-03-10 10:48:25 +0100

    Cheese model basic

--- everycheese/everycheese/cheeses/models.py
+++ everycheese/everycheese/cheeses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+# TimeStampedModel automatically gives the model created
+# and modified fields which automatically track when
+# the object is created or modified
+class Cheese(TimeStampedModel):
+    name = models.CharField('Name of Cheese', max_length=255)
+    slug = AutoSlugField('Cheese Address', unique=True, always_update=False, populate_from='name')
+    description = models.TextField('Description', blank=True)
